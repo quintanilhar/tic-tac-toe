72bea10ed23feb0be863554db38fd6851ff7e10e
Ricardo Quintanilha <ricardo.aquintanilha@gmail.com>
2017-06-22 08:25:58 -0300

    Add frontend for backend

---
+++ frontend/public/assets/js/game.js
@@ -0,0 +1,73 @@
+var Game = function () {
+};
+
+Game.prototype = {
+    constructor: Game,
+    humanTeam: 'X',
+    botTeam: 'O',
+
+    registerEvents: function (game) {
+        $('.board-cell')
+            .filter(function() {
+                return !$(this).find('input').val();
+            })
+            .on('click', function(e) {
+                game.move(
+                    $(this).data('row'),
+                    $(this).data('column'),
+                    game.humanTeam
+                );
+
+                $(this).unbind('click');
+
+                game.requestBotMove(game);
+            });
+    },
+
+    removeEvents: function () {
+        $('.board-cell').unbind('click');
+    },
+
+    move: function (xCoordinate, yCoordinate, team) {
+        $('input[name="board[' + xCoordinate + '][' + yCoordinate + ']"]')
+            .val(team)
+            .parent()
+            .append(team);
+    },
+
+    requestBotMove: function (game) {
+        this.removeEvents();
+
+        board = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+
+        $('.board-cell').each(function (index, item) {
+            board[$(this).data('row')][$(this).data('column')] = $(this).find('input').val();
+        });
+
+        $.ajax({
+            url: 'http://localhost:5010/v1/' + game.botTeam + '/moves',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({board: board}),
+        }).done(function (data) {
+            game.move(
+                data.x,
+                data.y,
+                game.botTeam        
+            );
+
+            game.registerEvents(game);
+        }).fail(function (e) {
+            alert('Something goes wrong, sorry, try again later.');
+        });
+    }
+};
+
+$(document).ready(function() {
+    var game = new Game();
+    game.registerEvents(game);
+});
